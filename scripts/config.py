5157b0a1a5deb3480c7df4651485755104272a6f
Nenrault <laurent.carnaroli@gmail.com>
2026-01-28 10:40:31 +0000

    fix

--- scripts/config.py
+++ scripts/config.py
@@ -9,7 +9,7 @@ TRAIN_DATA_PATH = os.path.join(DATA_FOLDER, 'la-haute-borne-data-2017-2020.csv')
 GENERATED_DATA_PATH = os.path.join(DATA_FOLDER,
                                    'la-haute-borne-data-2017-2020.csv')  # TODO change, should point to generator
 
-FEATURES_PATH = os.path.join(DATA_FOLDER, 'prepared_features.parquet')
+# FEATURES_PATH = os.path.join(DATA_FOLDER, 'prepared_features.parquet')
 
 MODEL_REGISTRY_FOLDER = os.path.join(PROJECT_FOLDER, 'models')
 MODEL_PATH = os.path.join(MODEL_REGISTRY_FOLDER, 'model.joblib')  # To change when needed
