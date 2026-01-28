f8b937a7f714355fccb69ba7753c129df833426c
Nenrault <laurent.carnaroli@gmail.com>
2026-01-28 10:34:35 +0000

    fix CI

--- dags/config.py
+++ dags/config.py
@@ -10,7 +10,7 @@ TRAIN_DATA_PATH = os.path.join(DATA_FOLDER, 'la-haute-borne-data-2017-2020.csv')
 # TODO change, should point to generator
 GENERATED_DATA_PATH = os.path.join(DATA_FOLDER, 'la-haute-borne-data-2017-2020.csv')
 
-FEATURES_PATH = os.path.join(DATA_FOLDER, 'prepared_features.parquet')
+# FEATURES_PATH = os.path.join(DATA_FOLDER, 'prepared_features.parquet')
 
 MODEL_REGISTRY_FOLDER = os.path.join(PROJECT_FOLDER, 'models')
 MODEL_PATH = os.path.join(MODEL_REGISTRY_FOLDER, 'model.joblib')  # To change when needed
