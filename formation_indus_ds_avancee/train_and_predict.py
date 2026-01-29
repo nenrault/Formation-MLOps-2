cc95cfa3376782c6b933543885d176b092e413bb
Nenrault <laurent.carnaroli@gmail.com>
2026-01-29 08:52:50 +0000

    linter

--- formation_indus_ds_avancee/train_and_predict.py
+++ formation_indus_ds_avancee/train_and_predict.py
@@ -18,7 +18,7 @@ def train_model(features: pd.DataFrame, model_registry_folder: str) -> None:
     y = features[target]
     with mlflow.start_run():
         mlflow.sklearn.autolog()
-        model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)        
+        model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)
         model.fit(X, y)
     time_str = time.strftime('%Y%m%d-%H%M%S')
     joblib.dump(model, os.path.join(model_registry_folder, time_str + '.joblib'))
