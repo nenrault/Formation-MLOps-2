9c7f3233af42d671c7332e0e74b549faec0533e8
Nenrault <laurent.carnaroli@gmail.com>
2026-01-29 10:07:28 +0000

    add log maybe

--- formation_indus_ds_avancee/train_and_predict.py
+++ formation_indus_ds_avancee/train_and_predict.py
@@ -21,6 +21,7 @@ def train_model(features: pd.DataFrame, model_registry_folder: str) -> None:
         model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)
         model.fit(X, y)
     time_str = time.strftime('%Y%m%d-%H%M%S')
+    mlflow.sklearn.log_model(model)
     joblib.dump(model, os.path.join(model_registry_folder, time_str + '.joblib'))
 
 
